f53988a4a6e75cd2a235832bfc0180411936fe4a
Yash Aryan <yasharyan307@outlook.com>
2019-10-02 22:38:23 +0530

    Update whatsappAPI.py

--- whatsappAPI.py
+++ whatsappAPI.py
@@ -5,7 +5,7 @@ with open("rawNumber.txt", "r") as g:
             numberOfPerson = line[:-1]
             header = "https://api.whatsapp.com/send?phone="
             number = "91"+numberOfPerson
-            message = "&text=Hello! This is a reminder message for an internship that you applied for, at the Innovation Center, MIT as a web developer/designer and with other proficient skills. Please be ready at 6:30pm tomorrow, i.e., Saturday at Co-working space, First Floor, IC."
+            message = "&text=your_message_here."
             q.write(header)
             q.write(number)
             q.write(message)
