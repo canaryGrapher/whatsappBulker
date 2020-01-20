540727815d9efbe10d01ece758a608493bd1428b
var-greyShader <yasharyan307@outlook.com>
2020-01-21 01:56:29 +0530

    major changes
    Performance and user experience made better

--- automateMessage.py
+++
@@ -1,9 +0,0 @@
-import webbrowser
-
-with open("file.txt", "r") as f:
-    for line in f:
-        print(line)
-        webbrowser.open(line)
-        waitConfirm = input("Enter confirmation command")
-        if waitConfirm == '\n':
-            continue
--- whatsappAPI.py
+++ whatsappAPI.py
@@ -1,11 +1,15 @@
+import webbrowser
+import time
+import os
+
+countryCode = input("Country code for this batch: ")
 with open("rawNumber.txt", "r") as g:
     numberPerson = g.readline()
     with open("file.txt", "w") as q:
         for line in g:
             numberOfPerson = line[:-1]
             header = "https://api.whatsapp.com/send?phone="
-            countryCode = input("Country code for this batch: ")
-            number = countryCode + numberOfPerson
+            number = "+" + str(countryCode) + numberOfPerson
             addMessage = "&text="
             message = "your_message_here."
             q.write(header)
@@ -13,6 +17,12 @@ with open("rawNumber.txt", "r") as g:
             q.write(addMessage)
             q.write(message)
             q.write("\n")
-            q.write("\n")
     q.close()
 g.close()
+
+with open("file.txt", "r") as f:
+    for line in f:
+        print(line)
+        webbrowser.open(line)
+        print("Enter confirmation command")
+        os.system("pause")
